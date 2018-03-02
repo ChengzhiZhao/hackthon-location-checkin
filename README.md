d8194d4d65ea943b8f4d5f6bd416a4142c52b218
yaofei <yaofei@meetup.com>
2018-03-02 11:33:43 -0500

    add parse for html and fix

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Hackthon project for location checkin
 
 Test Endpoint for calculating distance
 POST /geo
-curl -H "Content-Type: application/json" -X POST -d '{"data":{"locatio73.984721","longitude":"40.725980"},{"latitude":"-73.995741","longitude":"40.725980"}]}}' http://localhost:8000/geo
+curl -H "Content-Type: application/json" -X POST -d '{"data":{"locations":[{"latitude":"-73.984721","longitude":"40.725980"},{"latitude":"-73.995741","longitude":"40.725980"}]}}' http://localhost:8000/geo
--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 const express        = require('express');
 const bodyParser     = require('body-parser');
 const app            = express();
+const path           = require("path");
 
 const port = 8000;
 app.use(bodyParser.json());
 
 require('./app/routes')(app, {});
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index2.html'));
+    //__dirname : It will resolve to your project folder.
+  });
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
